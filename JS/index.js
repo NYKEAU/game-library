57902c4a74d7c6ca20921020189e52a5e46f88fb
NYKEAU <lhommeau.n@gmail.com>
2022-04-08 23:55:04 +0200

    Initial commit

---
+++ JS/index.js
@@ -0,0 +1,100 @@
+let games = {
+    "bs" : {
+        title: "Beat Saber",
+        content: `<p>
+        Beat Saber est une expérience rythmique immersive sans précédent ! Découvrez des tonnes de niveaux conçus à la main et laissez-vous happer par une musique électronique entraînante,
+        le tout dans un environnement futuriste. Utilisez vos sabres pour trancher les notes à mesure qu'elles arrivent vers vous. Chaque cube indique quel sabre vous devez utiliser et
+        dans quelle direction vous devez trancher. <h3>Caractéristiques</h3> <ul> <li><strong>Le rythme dans la peau</strong> : Plongez dans une atmosphère unique combinant
+        musique et effets visuels avec le gameplay unique de Beat Saber.<br> </li><li><strong>Musiques et niveaux originaux</strong> : Contrairement aux autres jeux de rythme dont le contenu est généré
+        aléatoirement, la musique et les niveaux de Beat Saber sont créés à la main pour vous offrir une expérience musicale unique.<br> </li><li><strong>Gardez le rythme en multijoueur</strong> :
+        Affrontez vos amis ou des inconnus du monde entier.<br> </li><li><strong>Une campagne stimulante</strong> : Améliorez-vous au quotidien en remplissant des objectifs et en surmontant des défis
+        dans la campagne.<br> </li><li><strong>Grimpez les échelons au classement</strong> : Affrontez d'autres joueuses et joueurs du monde entier dans différents modes de difficulté.<br>
+        </li><li><strong>Facile à prendre en main, amusant à maîtriser</strong> : Tout le monde comprendra les mécaniques de base du jeu. Il est simple à prendre en main et accessible à tous.<br>
+        </li><li><strong>Parfait pour garder la forme</strong> : Dansez et tranchez les notes, Beat Saber va vous faire bouger. </li></ul></p>`,
+        img: "IMG/beat-saber-vr.jpg"
+    },
+    "apex" : {
+        title: "Apex Legends",
+        content: `<p>
+        Dominez vos adversaires dans Apex Legends, un jeu de tir gratuit où des personnages légendaires aux capacités surpuissantes combattent ensemble aux confins de la Frontière
+        pour la gloire et la fortune.<br>Maîtrisez une équipe de Légendes en évolution constante avec une grande profondeur tactique et une kyrielle d'innovations qui enrichissent
+        votre expérience au sein d'un monde sauvage où tout peut arriver. Bienvenue dans la nouvelle évolution des jeux de tir de héros.<br><h3>Caractéristiques</h3>
+        <ul><li><strong>Une équipe de personnages légendaires</strong> : Maîtrisez une équipe évolutive de puissantes Légendes, faciles à prendre en main mais
+        exigeantes à totalement contrôler, qui possèdent toutes une personnalité, des forces et des capacités distinctes.<br></li><li><strong>Assemblez votre escouade</strong>
+        : Choisissez votre propre Légende et combinez ses aptitudes uniques à celles des autres joueurs afin de former le groupe de combattants ultime.<br></li><li><strong>Jeu
+        stratégique en équipe</strong> : Qu'il s'agisse de livrer bataille au sein d'une immense ville flottante ou de vous engager dans des combats rapprochés en mode Battle
+        Royale ou Arènes, vous devrez agir vite. Maîtrisez les capacités uniques de votre Légende et coordonnez-vous avec vos équipiers pour élaborer de nouvelles tactiques
+        et de puissantes combinaisons.<br></li><li><strong>Combattez différemment</strong> : Apprenez à maîtriser une sélection évolutive d'armes et d'équipements puissants.
+         Déplacez-vous rapidement et mettez à profit la cadence de tir de vos armes pour exploiter le plein potentiel de votre arsenal. Variez les plaisirs en prenant part
+        à des modes de jeu à durée limitée, et découvrez des tonnes de nouveau contenu à chaque saison.<br></li><li><strong>Un univers en évolution permanente</strong>
+        : Apex Legends se déroule dans un univers incroyablement immersif où l'histoire ne cesse jamais d'évoluer, les cartes changent à chaque saison et des Légendes
+        rejoignent régulièrement le combat. Marquez les Jeux Apex de votre empreinte avec une multitude de tenues distinctives et vivez une aventure inoubliable !
+        </li></ul></p>`,
+        img: "IMG/apex.jpg"
+    },
+    "rl" : {
+        title: "Rocket League",
+        content: `<p>Téléchargez le jeu et participez à ce mélange détonnant de football de style arcade et de chaos à quatre roues&nbsp;! Personnalisez votre voiture, 
+        lancez-vous sur le terrain et participez à l'un des jeux de sport les plus acclamés par la critique&nbsp;! Téléchargez le jeu et tirez votre coup d'envoi&nbsp;!
+        Lancez-vous sur le terrain seul ou avec vos amis dans les modes en ligne&nbsp;1v1, 2v2 et 3v3, ou bien profitez des modes Extra comme Rumble, Jour de neige et Paniers. 
+        Débloquez des objets du Rocket Pass, montez dans le classement, participez à des tournois compétitifs, relevez des défis, profitez de la progression multiplateforme et 
+        bien plus encore ! Le terrain vous attend. Tirez votre coup d'envoi&nbsp;!<h3>Caractéristiques</h3><ul><li><strong>Nouveaux défis</strong> : Participez aux défis hebdomadaires 
+        et saisonniers pour gagner des objets de personnalisation&nbsp;!</li><li><strong>Tournois</strong> : Vivez l'énergie de la compétition&nbsp;! Participez gratuitement à 
+        des tournois durant toute la saison contre des équipes de votre rang&nbsp;! Remportez de nouvelles récompenses&nbsp;!</li><li><strong>Événements en jeu et modes à durée 
+        limitée</strong> : De Reliques Hantées à Frosty Fest, profitez d'événements à durée limitée avec des objets en jeu festifs pouvant être débloqués en jouant en ligne&nbsp;! 
+        Restez à l'affût pour les modes et arènes à durée limitée.</li><li><strong>Progression multiplateforme</strong> : Partagez votre inventaire Rocket League, votre rang compétitif
+        et votre Rocket Pass tiers sur toutes vos plateformes connectées&nbsp;!</li><li><strong>Boutique d'objets et plans</strong> : Créez la voiture qui vous ressemble grâce à des 
+        options de personnalisation quasi illimitées&nbsp;! Obtenez des objets en jeu en remportant des défis, parcourez la boutique d'objets, ou utilisez des plans pour créer du 
+        contenu premium pour votre voiture.</li></ul></p>`,
+        img: "IMG/rl.jpg"
+    },
+    "exo" : {
+        title: "Exo One",
+        content: `<p>Un signal étrange... un vaisseau d'un autre monde... un voyage interplanétaire défiant la gravité, à travers le temps et l'espace.
+        Le jour de l’anniversaire de l’accident de Jupiter, un signal extraterrestre transmet à l’humanité les plans de construction d’un vaisseau alien.<br><br>Une fois construit, celui-ci
+        est baptisé… Exo One. Maîtrisez un système de navigation d’un autre monde et évoluez comme jamais auparavant à travers des paysages extraterrestres énigmatiques et
+        inhabités.Servez-vous de la gravité et de votre élan pour atteindre des vitesses hors du commun et des hauteurs phénoménales.<br><br>Élevez-vous dans les airs en surfant sur des 
+        courants d’air chaud jusqu’à atteindre des formations nuageuses bouillonnantes, zigzaguez le long de pentes vertigineuses et décollez depuis des hauteurs montagneuses sans 
+        jamais perdre de vue l’énigmatique rayon de lumière bleue qui brille à l’horizon.<br><br>Les graphismes aériens et envoûtants d'Exo One sont portés par une bande-son hypnotique 
+        composée de riffs planants de guitare électrique et d’effets sonores d’un autre monde. Prenez les commandes de votre vaisseau dans le cadre du premier voyage de 
+        l'humanité hors du système solaire, et partez pour un périple interplanétaire qui vous mènera aux confins du temps et de l'espace.</p>`,
+        img: "IMG/exo-one.jpg"
+    },
+    "mc" : {
+        title: "Minecraft",
+        content: `<p>Explorez des mondes générés aléatoirement et construisez tout ce qui vous passe par la tête, d'un abri de fortune au plus fabuleux des châteaux. Jouez en mode 
+        Créatif avec des ressources illimitées, ou creusez jusqu'au centre du monde pour fabriquer armes et armures afin de vaincre les dangereuses créatures du mode Survie. 
+        Gravissez des montagnes escarpées, découvrez des cavernes complexes et extrayez de grandes veines de minerai. Découvrez des biomes de cavernes luxuriantes et de spéléothème. 
+        Illuminez votre monde avec des bougies pour montrer à quel point vous êtes un spéléologue et un montagnard avisé&nbsp;!
+        
+        <h3>quaraktérystik</h3>
+        <ul>
+        <li><strong>Si vous en rêvez, vous pouvez le construire.</strong> Exploitez votre imagination et vos ressources illimitées avec le Mode Créatif.</li>
+        <li><strong>Combattez des créatures, construisez un abri et explorez le paysage</strong> : tout cela en une seule journée de travail, tandis vous essayez de survivre et de prospérer en Mode Survie.</li>
+        <li><strong>Vous pourrez explorer de nouveaux outils, lieux et espaces</strong> grâce à nos mises à jour régulières.</li>
+        <li><strong>Jouez en multijoueur multiplateforme pour un maximum de huit joueurs</strong> sur Windows, PlayStation, Nintendo, Xbox et appareils mobiles. Minecraft pour Windows s'exécute également sur les appareils Windows Mixed&nbsp;Reality et Oculus&nbsp;Rift et prend en charge toutes les fonctionnalités.</li>
+        <li><strong>Commandes slash</strong> : Ajustez le comportement du jeu&nbsp;: vous pouvez offrir des objets, invoquer des créatures, modifier l'heure du jour, etc.</li>
+        <li><strong>Découvrez les packs de skins, de textures et mash-up de la communauté !</strong> Pour en savoir plus, consultez <a target="_blank" href="https://www.minecraft.net/marketplace">minecraft.net/marketplace</a>. </li>
+        <li><strong>Rejoignez des serveurs massivement multijoueur et jouez avec des milliers d'autres joueurs !</strong> Découvrez des mondes gigantesques qui fonctionnent grâce à la communauté, prenez part à des mini-jeux uniques et échangez dans des salons peuplés de nouveaux amis.</li>
+        </ul></p>`,
+        img: "IMG/minecraft.webp"
+    }
+}
+
+function showGame(target, game) {
+    let dataGame = game ? games[game] : games[target.dataset.game];
+    let parent = document.getElementById("content-article")
+        parent.querySelector('h2').innerHTML = dataGame.title
+        parent.querySelector('p').innerHTML = dataGame.content
+        parent.querySelector('img').src = dataGame.img
+}
+
+window.onload = function() {
+    let tabs = document.getElementById("tabs");
+
+    for (let game in games) {
+        tabs.innerHTML += `<button data-game="${game}" onclick="showGame(this)" class="header_btn">${games[game].title}</button>`;
+    }
+
+    showGame("", Object.keys(games)[0]);
+}
